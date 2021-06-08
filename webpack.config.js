bbfdba4d7742eff424b886d97b0b5baf838b9939
reckj <reck.johannes@gmail.com>
2021-06-09 01:06:54 +0200

    first built

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,21 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css',
